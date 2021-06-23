b9a9066a4df83d75dbcb57a768458f580d9caf78
Thomas <thomastiercin@gmail.com>
2021-06-23 17:16:57 +0200

    Responsive / Font

--- src/App/Header.jsx
+++ src/App/Header.jsx
@@ -15,40 +15,42 @@ class Header extends React.Component {
     render() {
         const { username } = this.state;
         return (
-            
-            <header className="p-3 bg-dark text-white">
-                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"></a>
-
-                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="/" className="nav-link px-2 text-danger">YUMMI</a></li>
-                        {username &&
-                            <li><a href="/measures" className="nav-link px-2 text-white">Measures</a></li>
-                        }
-                        {username &&
-                            <li><a href="/ingredients" className="nav-link px-2 text-white">Ingredients</a></li>
-                        }
-                        {username &&
-                            <li><a href="/recipes" className="nav-link px-2 text-white">Recipes</a></li>
-                        }
-                        {username &&
-                            <li><a href="/recipeIngredients" className="nav-link px-2 text-white">Recipe Ingredients</a></li>
-                        }
-                    </ul>
-                    <div className="text-end">
-                        {username && <a className=" text-white text-decoration-none text-end">{username} &nbsp;&nbsp;</a>}
-                        {username &&
-                            <a href="/logout" className="btn btn-outline-danger me-2">Logout</a>
-                        }
-                        {!username &&  
-                            <a href="/login" className="btn btn-outline-light me-2">Login</a>
-                        }
-                        {!username &&  
-                            <a href="/signup" className="btn btn-outline-warning me-2">Sign-up</a>
-                        }             
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand text-danger" href="/">YUMMI</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarYummi" aria-controls="navbarYummi" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarYummi">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            {username &&
+                                <li><a href="/measures" className="nav-link px-2 text-white">Measures</a></li>
+                            }
+                            {username &&
+                                <li><a href="/ingredients" className="nav-link px-2 text-white">Ingredients</a></li>
+                            }
+                            {username &&
+                                <li><a href="/recipes" className="nav-link px-2 text-white">Recipes</a></li>
+                            }
+                            {username &&
+                                <li><a href="/recipeIngredients" className="nav-link px-2 text-white">Recipe Ingredients</a></li>
+                            }
+                        </ul>
+                        <div class="d-flex">
+                            {username && <a className="nav-link disabled px-2 text-white">{username} &nbsp;&nbsp;</a>}
+                            {username &&
+                                <a href="/logout" className="btn btn-outline-danger me-2">Logout</a>
+                            }
+                            {!username &&  
+                                <a href="/login" className="btn btn-outline-light me-2">Login</a>
+                            }
+                            {!username &&  
+                                <a href="/signup" className="btn btn-outline-warning me-2">Sign-up</a>
+                            }    
+                        </div>
                     </div>
                 </div>
-            </header>
+            </nav>
         );
     }
 }
